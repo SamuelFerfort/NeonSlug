76da4271fce46fda225c6f52c4b700d92f2b99c9
Samuel <samuferfort@gmail.com>
2024-12-16 21:41:17 +0100

    redirect to dashboard from main page if the user is logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import URLShortener from "@/src/components/layout/url-shortener";
 import PremiumFeatures from "../components/layout/premium-features";
+import { auth } from "../auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="min-h-screen  text-gray-100  flex flex-col">
       <main className="flex-grow container mx-auto px-4 py-14 flex flex-col items-center justify-center space-y-16">
