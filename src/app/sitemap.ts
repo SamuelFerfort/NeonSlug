b716dfcc3390040bbb8bd2aca9f5508a1f4bc1d4
Samuel <samuferfort@gmail.com>
2024-12-18 22:49:25 +0100

    ✨ add: implement sitemap generation for SEO optimization

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const currentDate = new Date().toISOString();
+
+  return [
+    {
+      url: "https://neonslug.com",
+      lastModified: currentDate,
+      priority: 1,
+    },
+    {
+      url: "https://neonslug.com/login",
+      lastModified: currentDate,
+      priority: 0.8,
+    },
+    {
+      url: "https://neonslug.com/dashboard",
+      lastModified: currentDate,
+      priority: 0.8,
+    },
+  ];
+}
